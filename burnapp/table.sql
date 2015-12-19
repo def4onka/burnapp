2a6be74a862236570743ef52e45c270e3a2b3b10
Porik Yana <purik.yana@yandex.ru>
2015-12-20 01:00:01 +0300

    sql reloaded

--- burnapp/table.sql
+++ burnapp/table.sql
@@ -1,14 +1,19 @@
-create table if not exists Tasks(
+create table if not exists tasks(
 	id serial primary key,
 	title text not null unique,
 	note text,
 	labor_vol integer not null,
 	status text not null check(status in ('Выполнено','Запланировано')),
-	readyday date check(status = 'Выполнено' or null)
-	);
+	readyday date check(status = 'Выполнено' and readyday is not null or status = 'Запланировано' and readyday is null)
+);
 
 create table if not exists workdays(
 	id serial primary key,
 	workday date not null unique,
 	labor_vol int not null
 );
+
+create table if not exists sprintdates(
+	id serial primary key,
+	begindate date not null,
+	enddate date not null);
